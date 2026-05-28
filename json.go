876a07310aa21f975a069a8e82868aadf7205528
Evgeni Pochechuev <ipchchv@gmail.com>
2026-05-28 09:07:30 +0000

    feat(json): implement the SaveJSONToFile function that saves data to a JSON file

--- json.go
+++ json.go
@@ -1,6 +1,9 @@
 package gojc
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"os"
+)
 
 // ToJSON converts any struct into a native JSON byte slice.
 func ToJSON(v any) ([]byte, error) {
@@ -10,4 +13,14 @@ func ToJSON(v any) ([]byte, error) {
 	}
 
 	return bytes, nil
+}
+
+// SaveJSONToFile writes the JSON byte slice into a
+// file with the specified name.
+func SaveJSONToFile(filename string, data []byte) error {
+	if err := os.WriteFile(filename, data, 0644); err != nil {
+		return err
+	}
+
+	return nil
 }
