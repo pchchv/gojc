c9ce33e378ab2a7891d77bea06f64c8b0198ee64
Evgeni Pochechuev <ipchchv@gmail.com>
2026-05-28 09:21:53 +0000

    refactor(json): update the SaveJSONToFile function to append data to the file

--- json.go
+++ json.go
@@ -15,10 +15,25 @@ func ToJSON(v any) ([]byte, error) {
 	return bytes, nil
 }
 
-// SaveJSONToFile writes the JSON byte slice into a
-// file with the specified name.
+// SaveJSONToFile writes the JSON byte slice into a file with the specified name.
 func SaveJSONToFile(filename string, data []byte) error {
-	if err := os.WriteFile(filename, data, 0644); err != nil {
+	// Open file with flags:
+	//   Append
+	//   Create if non-existent
+	//   Write-Only
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write the JSON data payload
+	if _, err := file.Write(data); err != nil {
+		return err
+	}
+
+	// Append a newline character to delimit multiple JSON objects properly
+	if _, err := file.Write([]byte("\n")); err != nil {
 		return err
 	}
 
