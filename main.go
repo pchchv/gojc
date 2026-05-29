f5c743c5c4fa50fd6b7f826679083065b7d70545
Evgeni Pochechuev <ipchchv@gmail.com>
2026-05-29 08:48:55 +0000

    feat: implement value retrieval

--- main.go
+++ main.go
@@ -1,3 +1,27 @@
 package gojc
 
+import (
+	"log"
+	"os"
+
+	"github.com/pchchv/env"
+)
+
+func init() {
+	// Load values from .env into the system
+	if err := env.Load(); err != nil {
+		log.Panic("no .env file found")
+	}
+}
+
+func getEnvValue(v string) string {
+	// Getting a value
+	// Outputs a panic if the value is missing
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panicf("value %v does not exist", v)
+	}
+	return value
+}
+
 func main() {}
