c3a570c7ad119c5d00be743d99489fdb804af419
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-03 18:05:28 +0000

    test(server): add load test for ping handler

--- server_test.go
+++ server_test.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 	"strings"
 	"testing"
+	"time"
+
+	vegeta "github.com/tsenart/vegeta/lib"
 )
 
 func TestServerPing(t *testing.T) {
@@ -33,4 +37,20 @@ func TestServerPing(t *testing.T) {
 	if !strings.Contains(b, "JSON") {
 		t.Fatal()
 	}
+}
+
+func TestLoadPing(t *testing.T) {
+	rate := vegeta.Rate{Freq: 1000, Per: time.Second}
+	duration := 5 * time.Second
+	targeter := vegeta.NewStaticTargeter(vegeta.Target{
+		Method: "GET",
+		URL:    "http://" + getEnvValue("HOST") + ":" + getEnvValue("PORT") + "/ping",
+	})
+	attacker := vegeta.NewAttacker()
+	var metrics vegeta.Metrics
+	for res := range attacker.Attack(targeter, rate, duration, "Big Bang!") {
+		metrics.Add(res)
+	}
+	metrics.Close()
+	log.Printf("99th percentile: %s\n", metrics.Latencies.P99)
 }
